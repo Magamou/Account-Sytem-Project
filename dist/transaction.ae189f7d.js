b7956d3f6d5ed5a231dbf1e9632edc6333298b3c
Magamou Gueye <gueyemagamou@gmail.com>
2021-12-09 07:02:51 +0000

    commit 09/12/2021 test.js, edit and transaction

--- dist/transaction.ae189f7d.js
+++ dist/transaction.ae189f7d.js
@@ -472,7 +472,19 @@ button.addEventListener('click', function(e) {
     for (h of tite)h.style.display = "none";
     menu.style.width = "5%";
     main.style.width = "95%";
-});
+}); // export const Selector={
+ //   menu:document.querySelector('#menu_context'),
+ //   state:0,
+ //   store:function(){
+ //     localStorage.setItem('state',`${this.state}`);
+ //   },
+ //   restore:function(name){
+ //     console.log(localStorage.getItem(`${name}`));
+ //   }
+ // }
+ // Selector.menu.style.backgroundColor="green";
+ // Selector.store();
+ // Selector.restore('state');
 
 },{}]},["enqa6","1wubG"], "1wubG", "parcelRequire3fe4")
 
--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 //     row.style.backgroundColor="yellow";
 //     row.style.minWidth
 // })
+
 let menu=document.querySelector('#menu_context');
 
 let button=menu.querySelector('button');
@@ -14,5 +15,18 @@ button.addEventListener('click', function(e){
     }
   menu.style.width="5%"; 
   main.style.width="95%";
-})
+});
  
+// export const Selector={
+//   menu:document.querySelector('#menu_context'),
+//   state:0,
+//   store:function(){
+//     localStorage.setItem('state',`${this.state}`);
+//   },
+//   restore:function(name){
+//     console.log(localStorage.getItem(`${name}`));
+//   }
+// }
+// Selector.menu.style.backgroundColor="green";
+// Selector.store();
+// Selector.restore('state');
